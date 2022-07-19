a0e851518d51f7b1279efb19e01312090a68f5dd
rohitprofc <rohitprofc@gmail.com>
2022-07-19 16:40:36 +0530

    Jul 19

--- e.py
+++ e.py
@@ -1,13 +1,15 @@
 # Displaying grade of student obtained by 5 subjects marks
+from numpy import average
 
+
+subject = []
 name = input('Enter student name: ')
-s1 = int(input('Enter marks in subject 1: '))
-s2 = int(input('Enter marks in subject 2: '))
-s3 = int(input('Enter marks in subject 3: '))
-s4 = int(input('Enter marks in subject 4: '))
-s5 = int(input('Enter marks in subject 5: '))
-avg = (s1+s2+s3+s4+s5)/5
-print('Grade of', name, 'is:',end = ' ')
+for i in range(5):
+    marks = int(input("Enter marks of subject "+str(i+1)+": "))
+    subject.append(marks)
+
+avg = average(subject)
+print('Grade of', name, 'is:', end=' ')
 if avg >= 90:
     print('O')
 elif avg >= 80:

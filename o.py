d385545c9ddf6f19558b1da893a696d60690f39b
rohitprofc <rohitprofc@gmail.com>
2022-07-21 14:57:25 +0530

    Jul 21

--- o.py
+++ o.py
@@ -7,7 +7,20 @@ for i in range(noOfElements):
     ele = int(input("Enter element "+str(i+1)+": "))
     lst.append(ele)
 
-print("List:",lst)
+print("List:", lst)
 print("Mean of given list: ", mean(lst))
 print("Median of given list: ", median(lst))
 print("Mode of given list: ", mode(lst))
+
+'''
+Output: -
+Enter no.of elements: 4
+Enter element 1: 4
+Enter element 2: 5
+Enter element 3: 5
+Enter element 4: 1
+List: [4, 5, 5, 1]
+Mean of given list:  3.75
+Median of given list:  4.5
+Mode of given list:  5
+'''
